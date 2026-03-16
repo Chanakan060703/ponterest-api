34934fb8d92453efd3e5991092f4c6f8ef3dd808
chanakan <chonakankongyen@gmail.com>
2026-03-17 02:11:46 +0700

    feat: parse auth cookie

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import {config} from 'dotenv';
+import cookieParser from "cookie-parser";
 import { connectDB, disconnectDB } from './config/db.js';
 
 import userRoutes from './routers/userRoutes.js';
@@ -14,6 +15,7 @@ await connectDB();
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
