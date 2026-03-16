d196f2e1cad83e87ac36500da32d96d0be725029
chanakan <chonakankongyen@gmail.com>
2026-03-17 02:06:35 +0700

    feat: images CRUD with filters and soft delete

--- src/routers/imageRoutes.js
+++ src/routers/imageRoutes.js
@@ -1,10 +1,14 @@
 import express from "express";
+import { authMiddleware } from "../middleware/authMiddleware.js";
+import { createImage, deleteImage, getImageById, getImages, updateImage } from "../controllers/imageController.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.status(501).json({ error: "Not implemented" });
-});
+router.get("/", getImages);
+router.get("/:id", getImageById);
+
+router.post("/", authMiddleware, createImage);
+router.put("/:id", authMiddleware, updateImage);
+router.delete("/:id", authMiddleware, deleteImage);
 
 export default router;
-
